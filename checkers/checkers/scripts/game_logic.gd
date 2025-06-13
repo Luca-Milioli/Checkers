630b1113ad38ecf3295525fb9d5d5a8e20f56a67
FedericoRigolon <318199@studenti.unimore.it>
2025-06-13 16:26:38 +0200

    updateGrid

---
+++ checkers/checkers/scripts/game_logic.gd
@@ -0,0 +1,65 @@
+class_name GameLogic
+
+const COLUMNS = 8
+const ROWS = 8
+enum {PLAYER_MAN, IA_MAN, NO_MAN}
+var board = []
+var player_turn : bool
+var winner : int
+
+signal update_gui
+
+func setup_matrix() -> void:
+	_create_matrix(COLUMNS, ROWS)
+	for y in range(COLUMNS):
+		for x in range(ROWS):
+			board[x][y] = NO_MAN
+			if (not bool((x + y)%2)):
+				if y < 3:
+					board[x][y] = PLAYER_MAN
+				elif y > 4:
+					board[x][y] = IA_MAN
+
+func print_board():
+	print("PLAYER: 0\nIA: 1\nVOID: 2\n")
+	for i in range(COLUMNS):
+		print("\nCOLUMNS "+str(i+1)+": ")
+		print(board[i])
+
+func _create_matrix(cols, rows):
+	for y in range(cols):
+		var row = []
+		for x in range(rows):
+			row.append(null)
+		self.board.append(row)
+
+func _check_move(old: Vector2i, new: Vector2i) -> bool:
+	return true
+
+func _set_move(old: Vector2i, new: Vector2i):
+	self.board[old[0]][old[1]] = self.board[new[0]][new[1]]
+	
+func _change_turn():
+	self.player_turn = !player_turn
+
+func _make_move():
+	var move_passed = false
+	var old_cell : Vector2i
+	var new_cell : Vector2i
+	while not move_passed:
+		old_cell = Vector2i(0, 0)
+		new_cell = Vector2i(0, 1)
+		move_passed = _check_move(old_cell, new_cell)
+	_set_move(old_cell, new_cell)
+
+func _check_winner() -> int:
+	return 1
+	
+func game_start():
+	while not self.winner:
+		_change_turn()
+		_make_move()
+		self.winner = _check_winner()
+		
+		
+		
